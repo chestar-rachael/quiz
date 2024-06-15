2f0ab1e0966254f319e27edc7a17bf1fa4246b3e
chestar-rachael <rebahrachael@gmail.com>
2024-06-15 21:18:16 +0300

    corrected JS

--- QUIZ/quiz.js
+++ QUIZ/quiz.js
@@ -1,55 +1,78 @@
-const data =[
-    
-    id, 1,
-    question, "which of these is actually a fish?",
-    answers,[
-        {answer:"swordfish",isCorrect: true},
-        {answer:"jellyfish",isCorrect: false},
-        {answer:"starfish",isCorrect: false},
-        {answer:"crayfish",isCorrect: false},
-    
-    ]
-      ]
-    
+const data = [
     {
-
-
-    id, 2,
-    question, "A flutter is a group of,"
-    answers,[
-        {answer: "bees",isCorrect: true},
-        {answer: "pengui",isCorrect: false},
-        {answer: "butterflies",isCorrect: true},
-        {answer: "camels",isCorrect: true },
-    ]
+        id: 1,
+        question: "Which of these is actually a fish?",
+        answers: [
+            { answer: "swordfish", isCorrect: true },
+            { answer: "jellyfish", isCorrect: false },
+            { answer: "starfish", isCorrect: false },
+            { answer: "crayfish", isCorrect: false },
+        ]
+    },
+    {
+        id: 2,
+        question: "A flutter is a group of:",
+        answers: [
+            { answer: "bees", isCorrect: false },
+            { answer: "penguins", isCorrect: false },
+            { answer: "butterflies", isCorrect: true },
+            { answer: "camels", isCorrect: false },
+        ]
+    },
+    {
+        id: 3,
+        question: "A group of which animals is referred to as a wake?",
+        answers: [
+            { answer: "bats", isCorrect: false },
+            { answer: "vultures", isCorrect: true },
+            { answer: "ants", isCorrect: false },
+        ]
     }
-
-{
-    [
-    id, 1,
-    question, "A group of which animals is referred to as a wake?",
-    answers,[
-        {answer: "bats",isCorrect: false},
-        {answer: "vultures",isCorrect: true},
-        {answer: "ants",isCorrect: false},
-    ],
-    ]
-}
+];
 
 
-const gameScreen =document.querySelector("game")
-const resultSceen =document.querySelector("results")
-const questions =document.querySelector("questions")
-const answersContainer =document.querySelector("answers")
-const submit =document.querySelector("submit")
-const play =document.querySelector("play")
+
+const gameScreen = document.querySelector(".game")
+const resultSceen = document.querySelector(".results")
+const question = document.querySelector(".question")
+const answersContainer = document.querySelector(".answers")
+const submit = document.querySelector(".submit")
+const play = document.querySelector(".play")
 
 let qIndex = 0
-let correctCount= 0
-let wrongCount= 0
-let total= 0
+let correctCount = 0
+let wrongCount = 0
+let total = 0
 let selectedAnswer;
 
-const showQuestion = (qNumber)=>{
-question.textContent =data[qNumber].question}
+const showQuestion = (qNumber) => {
+    selectedAnswer = null;
+    question.textContent = data[qNumber].question
+    answersContainer.innerHTML = data[qIndex].answers.map((item, index) => `<div class="answers">
+<input name="answer" type="radio" id=${index} name="answer" value=${item.iscorrect}>
+<label for="1">${item.answer}</label>
+</div>`
 
+    ).join("");
+
+    selectAnswer()
+};
+const selectAnswer = () => {
+    answersContainer.querySelectorAll("input").forEach(el => {
+        el.addEventListener("click", (e) => {
+            selectedAnswer = e.target.value
+        })
+    })
+}
+
+const submitAnswer = () => {
+    submit.addEventListener("click", () => {
+        if (selectedAnswer !== null) {
+            selectedAnswer === "true" ? correctAnswer++ : wrongCount++
+            qIndex++
+            showQuestion(qIndex)
+        } else alert("select an answer")
+    })
+}
+showQuestion(qIndex);
+submitAnswer()
